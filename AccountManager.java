d80e68b4261008a592f7e9dbd9dcd3462d410cbd
Dagi93 <dagi.xd@hotmail.de>
2014-03-26 10:48:14 +0100

    Acc und AccImpl update

--- AccountManager.java
+++ AccountManager.java
@@ -1,4 +1,13 @@
 
-public class AccountManager {
-
+public interface AccountManager {
+    void newPlayer(String name);
+    void buy(String PlayerName, String shareName, int amount);
+    void sell(String PlayerName, String shareName, int amount);
+    long getAssetValue(Asset asset);
+    long getPlayerAllAssets(Player player);
+    long getShareValue(Share share);
+    String allSharesToString();
+    
+    
+    
 }
---
+++ AccountManagerImpl.java
@@ -0,0 +1,47 @@
+
+public class AccountManagerImpl implements AccountManager{
+    Share[] shareCollection = new Share[10];
+
+    @Override
+    public void newPlayer(String name) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void buy(String PlayerName, String shareName, int amount) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void sell(String PlayerName, String shareName, int amount) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public long getAssetValue(Asset asset) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public long getPlayerAllAssets(Player player) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public long getShareValue(Share share) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String allSharesToString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
